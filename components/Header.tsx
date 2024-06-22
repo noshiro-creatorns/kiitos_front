4a38d6af71ac81ba77288e2a911043135e4f6255
noshiro-creatorns <creatorns2020@gmail.com>
2024-06-22 20:10:59 +0900

    logo test up

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logoImage1 from '../public/static/logo-1.png';
+import logoImage2 from '../public/static/logo-2.png';
+import logoImage3 from '../public/static/logo-3.png';
+import styles from './HamburgerMenu.module.css';
 
 const Header: React.FC = () => {
   return (
     <header className="border-b flex items-center h-14 px-4 bg-blue-base">
       <h1>
-        <Link href="/home" className="text-2xl font-logo">kiitos</Link>
+        <Link href="/home" className="text-2xl font-logo">
+            <Image src={logoImage1} width={100} height={20} alt="logo" className={`${styles.menu_icon}`}/>
+            <Image src={logoImage2} width={100} height={20} alt="logo" className={`${styles.menu_icon}`}/>
+            <Image src={logoImage3} width={100} height={20} alt="logo" className={`${styles.menu_icon}`}/>
+        </Link>
       </h1>
     </header>
   );
